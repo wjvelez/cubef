3fd41f767d0cba51821b98373601537e465f5518
Jamil Velez <wjvelezn@gmail.com>
2022-02-24 00:03:57 -0500

    added example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:cubef/cubef.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,98 +13,54 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Cubef Widget Example'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+const double _width = 280.0;
+const double _height = 280.0;
 
-  GlobalKey<CubefState> cubefKey = new GlobalKey<CubefState>();
-  
+class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey<CubefState> cubefKey = GlobalKey<CubefState>();
+
   void rollDown() {
-    cubefKey.currentState.rollDown();
+    cubefKey.currentState!.rollDown();
   }
 
   void rollUp() {
-    cubefKey.currentState.rollUp();
+    cubefKey.currentState!.rollUp();
   }
 
   void rollLeft() {
-    cubefKey.currentState.rollLeft();
+    cubefKey.currentState!.rollLeft();
   }
 
   void rollRight() {
-    cubefKey.currentState.rollRight();
+    cubefKey.currentState!.rollRight();
   }
 
-  static double _width = 100.0, _height = 100.0;
-
   List<Widget> sides = [
-    Container(
-      height: _height,
-      width: _width,
-      decoration: BoxDecoration(
-        color: Colors.black
-      ),
-      child: Text("1"),
-    ),
-    Container(
-      height: _height,
-      width: _width,
-      decoration: BoxDecoration(
-        color: Colors.green[500],
-      ),
-      child: Text("2"),
-    ),
-    Container(
-      height: _height,
-      width: _width,
-      decoration: BoxDecoration(
-        color: Colors.orange[500],
-      ),
-      child: Text("3"),
-    ),
-    Container(
-      height: _height,
-      width: _width,
-      decoration: BoxDecoration(
-        color: Colors.blue[500],
-      ),
-      child: Text("4"),
-    ),
-    Container(
-      height: _height,
-      width: _width,
-      decoration: BoxDecoration(
-        color: Colors.pink[500],
-      ),
-      child: Text("5"),
-    ),
-    Container(
-      height: _height,
-      width: _width,
-      decoration: BoxDecoration(
-        color: Colors.purple[500],
-      ),
-      child: Text("6"),
-    ),
+    const SideContainer(label: '1', color: Colors.indigo),
+    SideContainer(color: Colors.green[500]!, label: '2'),
+    SideContainer(color: Colors.orange[500]!, label: '3'),
+    SideContainer(color: Colors.blue[500]!, label: '4'),
+    SideContainer(color: Colors.pink[400]!, label: '5'),
+    const SideContainer(color: Colors.purple, label: '6')
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Cubef Widget Example'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -114,16 +72,81 @@ class _MyHomePageState extends State<MyHomePage> {
             width: _width,
             height: _height,
           ),
-          Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-            RaisedButton(onPressed: rollUp, child: Text('Roll Up')),
-            RaisedButton(onPressed: rollDown, child: Text('Roll Down')),
-            RaisedButton(onPressed: rollLeft, child: Text('Roll Left')),
-            RaisedButton(onPressed: rollRight, child: Text('Roll Right')),
-          ],)
+          const SizedBox(height: 32),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ElevatedButton(
+                        onPressed: rollUp,
+                        child: const Text('Roll Up'),
+                      ),
+                      ElevatedButton(
+                        onPressed: rollDown,
+                        child: const Text('Roll Down'),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ElevatedButton(
+                        onPressed: rollLeft,
+                        child: const Text('Roll Left'),
+                      ),
+                      ElevatedButton(
+                        onPressed: rollRight,
+                        child: const Text('Roll Right'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
+      ),
+    );
+  }
+}
+
+class SideContainer extends StatelessWidget {
+  const SideContainer({
+    Key? key,
+    required this.label,
+    this.color = Colors.blue,
+  }) : super(key: key);
+
+  final String label;
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: _height,
+      width: _width,
+      decoration: BoxDecoration(
+        color: color,
+      ),
+      child: Center(
+        child: Text(
+          label,
+          style: const TextStyle(
+            fontSize: 26,
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
